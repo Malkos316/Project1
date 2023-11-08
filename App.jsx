41ea509401e8b707c423f61f13c05281f6481327
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-11-08 15:28:42 -0700

    Lab 3 submission

--- App.jsx
+++ App.jsx
@@ -4,15 +4,25 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native';
 import ToDoList from './src/ToDoList';
 import ToDoForm from './src/ToDoForm';
 
 function App() {
+
+  const [tasks, setTasks] = useState(
+    [
+      'Do laundry',
+      'Go to gym',
+      'Walk dog',
+      'Study React Native'
+    ]
+  );
+
   return (
   <SafeAreaView>
-    <ToDoList />
+    <ToDoList tasks = {tasks} />
     <ToDoForm />
   </SafeAreaView>
 
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,23 +1,18 @@
+import React from 'react';
 import { StyleSheet, ScrollView, Pressable, View, Text} from 'react-native';
-export default function ToDoList() {
+export default function ToDoList({tasks = []}) {
     return (
         <>
             <ScrollView>
-                <Pressable>
-                    <View style={[styles.task, styles.completed]}>
-                        <Text style={styles.taskText}>Do laundry</Text>
-                    </View>
-                </Pressable>
-                <Pressable>
-                    <View style={[styles.task]}>
-                        <Text style={styles.taskText}>Go to gym</Text>
-                    </View>
-                </Pressable>
-                <Pressable>
-                    <View style={[styles.task, styles.completed]}>
-                        <Text style={styles.taskText}>Walk dog</Text>
-                    </View>
-                </Pressable>
+                {tasks.map((task, index) => (
+                    <React.Fragment key={index}>
+                        <Pressable>
+                            <View style={[styles.task]}>
+                                <Text style={styles.taskText}>{task}</Text>
+                            </View>
+                        </Pressable>
+                    </React.Fragment>
+                ))}
             </ScrollView>
         </>
     );
